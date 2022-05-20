b3701bd12fd714293e6c20674995dfea457a676f
TaeWonYoon <60877522+TaeWonYoon@users.noreply.github.com>
2022-05-20 13:56:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -54,5 +54,10 @@ function callAjax(formData, url, callback, async) {
 	})
 }
 
-//폰 - 
+//폰 - 정규식
 $(".phoneNumber").keyup(function() { $(this).val( $(this).val().replace(/[^0-9]/g, "").replace(/(^02|^0505|^1[0-9]{3}|^0[0-9]{2})([0-9]+)?([0-9]{4})$/,"$1-$2-$3").replace("--", "-") ); });
+
+//콘솔 로그
+function log(name,data) {
+	console.log(name+" = " +  JSON.stringify(data))	
+}
